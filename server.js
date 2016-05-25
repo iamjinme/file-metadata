7cab0336eb0165b72d0dd0d87b737a0304a1effe
Jinme Mirabal <mirabalj@gmail.com>
2016-05-25 12:21:28 -0400

    Quit Mongodb dependency and change README file

--- server.js
+++ server.js
@@ -1,28 +1,18 @@
 'use strict';
 
 var express = require('express'),
-    routes = require('./app/routes/index.js'),
-    mongoose = require('mongoose');
+    routes = require('./app/routes/index.js')
 
 var app = express();
 require('dotenv').load();
 
-var mongo_uri = process.env.MONGO_URI || 'mongodb://localhost/test';
-mongoose.connect(mongo_uri);
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 
-var db = mongoose.connection;
+routes(app);
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-
-  app.use('/public', express.static(process.cwd() + '/public'));
-  app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-
-  routes(app);
-
-  var port = process.env.PORT || 8080;
-  app.listen(port, function () {
-    console.log('Node.js listening on port ' + port + '...');
-  });
-  
+var port = process.env.PORT || 8080;
+app.listen(port, function () {
+  console.log('Node.js listening on port ' + port + '...');
 });
+  
