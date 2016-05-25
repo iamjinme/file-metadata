d89e475d162462f26de6b17b3b766fba69d9eaa9
Jinme Mirabal <mirabalj@gmail.com>
2016-05-25 12:01:39 -0400

    Delete unnecesary files, fix upload path

--- app/controllers/fileUpload.server.js
+++ app/controllers/fileUpload.server.js
@@ -4,9 +4,10 @@ var multer = require('multer');
 
 function FileUpload() {
   
-  var upload = multer({ dest: __dirname + "/public/upload"}).single('file');
+  var upload = multer({ dest: __dirname + "../../public/upload"}).single('file');
   
   this.getInfo = function(req, res) {
+    console.log(__dirname);
     upload(req,res,function(err) {
       if(err) {
         return res.json({"error": true});
